1d8ab6d038b24ff770c7d345fab845f1f295a7e3
BigMickey69 <williamclivestevens1205@gmail.com>
2024-07-14 15:18:00 +0800

    Added NEAT and a very simple neural network for flappy-bird

--- AI-flappy-bird/main.py
+++ AI-flappy-bird/main.py
@@ -2,6 +2,33 @@ import pygame as pg
 import os
 import time
 from classes import Bird, Pipe, Ground
+import neat
+
+config_path = os.path.join("NEAT-config.txt")
+
+def neat_setup(config_path):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation, 
+                         config_path)
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(show_species_detail=True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main,25)
+    print("======================================")
+    print(f"Best genome is: {winner}")
+    print("======================================")
+
+
+
+
+
+
+WHITE = (0,0,0)
+BLACK = (255,255,255)
+Gen = -1
 
 pg.font.init()
 FONT = pg.font.SysFont("comicsans", 45)
@@ -16,25 +43,31 @@ WIN = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pg.display.set_caption("Flappy Bird")
 
 
+def draw_outline_text(win, FONT, text,x,y):
+    inner = FONT.render(text, 1, BLACK)
+    outer = FONT.render(text, 1, WHITE)
+    win.blit(outer, (x-2,y-2))
+    win.blit(outer, (x-2,y+2))
+    win.blit(outer, (x+2,y-2))
+    win.blit(outer, (x+2,y+2))
+    win.blit(inner, (x,y))
 
 draw_count = 0
 bg_y = 0
 draw_cycle = 0
 bg_breath = 3
-def draw_score(win, score):
-    WHITE = (0,0,0)
-    BLACK = (255,255,255)
+def draw_score_gen(win, score):
+    global Gen
+    
     inner_score = FONT.render(f"Score: {score}", 1, BLACK)
-    outer_score = FONT.render(f"Score: {score}", 1, WHITE)
     x = WIN_WIDTH - 10 - inner_score.get_width()
-    y = 10
-    win.blit(outer_score, (x-2,y-2))
-    win.blit(outer_score, (x-2,y+2))
-    win.blit(outer_score, (x+2,y-2))
-    win.blit(outer_score, (x+2,y+2))
-    win.blit(inner_score, (x,y))
+    draw_outline_text(win, FONT, f"Score: {score}", x, 10)
 
-def draw_window(win, bird, pipes, ground, score):
+    inner_gen = FONT.render(f"Gen: {Gen}", 1, BLACK)
+    draw_outline_text(win, FONT, f"Gen: {Gen}", 10, 10)
+    
+
+def draw_window(win, birds, pipes, ground, score):
     global draw_count, bg_y, draw_cycle, bg_breath
 
     
@@ -57,23 +90,40 @@ def draw_window(win, bird, pipes, ground, score):
         pipe.draw(win)
     ground.move()
     ground.draw(win)
-    bird.draw(win)
-    draw_score(win, score)
+
+    for bird in birds:
+        bird.draw(win)
+
+    draw_score_gen(win, score)
     
     pg.display.update()
 
 
-def main():
-    score = 0
-    add_pipe = False
+def main(genomes, config):
+    global Gen
+    Gen += 1
+    birds = []
+    nets = []
+    ge = []
+
+    for _ , g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g,config)
+        nets.append(net)
+        birds.append(Bird(200,200))
+        g.fitness = 0
+        ge.append(g)
+
+
+
+    
+    
 
 
     clock = pg.time.Clock()
-    birdy = Bird(200,200)
     ground = Ground(790)
     pipes = [Pipe(700)]
 
-
+    score = 0
     running = True
 
     while running:
@@ -81,46 +131,77 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    birdy.jump()
-                if event.key == pg.K_ESCAPE:
-                    running = False
+                pg.quit()
+                quit()
 
+        pipe_index = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].TOP_PIPE.get_width():
+                    pipe_index = 1
+        else:
+            running = False
+            print("!!! No birds left, moving to new gen !!!")
+            break   
+            
+        for i, bird in enumerate(birds):
+            bird.move()
+            ge[i].fitness += 0.05
+
+            output = nets[i].activate((bird.y, abs(bird.y - pipes[pipe_index].top_height), abs(bird.y - pipes[pipe_index].bottom)))
+            if output[0] > 0.5:
+                bird.jump()
+
+
+        add_pipe = False
         remove = []
         for pipe in pipes:
-            if pipe.collide(birdy):
-                print("Collided, game over QQ")
+            pipe.move()
+
+            for i, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[i].fitness -= 3
+                    birds.pop(i)
+                    nets.pop(i)
+                    ge.pop(i)
+                    print("Bird died to pipe!")
+                    
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+                
 
             if pipe.x + pipe.BOTTOM_PIPE.get_width() < 0:
                 remove.append(pipe)
 
-            if not pipe.passed and pipe.x < birdy.x:
-                pipe.passed = True
-                add_pipe = True
-            pipe.move()
-            
-
         if add_pipe:
             score +=1
-            print(f"Current score: {score}")
+            for g in ge:
+                g.fitness += 5
+
             pipes.append(Pipe(620))
             add_pipe = False
 
         for pipe in remove:
             pipes.remove(pipe)
 
-        birdy.move()
+        for i, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 790:
+                birds.pop(i)
+                nets.pop(i)
+                ge.pop(i)
+                print("bird died to ground QQ")
+            elif bird.y <0:
+                birds.pop(i)
+                nets.pop(i)
+                ge.pop(i)
+                print("bird flew too high QQ")
 
-        if birdy.y + birdy.img.get_height() >= 790:
-            print("you ded, game over")
+        draw_window(WIN, birds, pipes, ground, score)
 
-        draw_window(WIN, birdy, pipes, ground,score)
 
-    pg.quit()
 
 if __name__ == "__main__":
-    main()
+    neat_setup(config_path)
 
 
 
